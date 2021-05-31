e401a195d73dc9a711877c91637b23b799b29a89
beecart <85115966+beecart@users.noreply.github.com>
2021-05-31 20:03:12 +0530

    Update imageList.js

--- imageList.js
+++ imageList.js
@@ -1,15 +1,15 @@
 function  getImageList() {
     return [
       {
-        url: 'https://raw.githubusercontent.com/sidhupk/api/main/beecart/assets/bee%20cart%20Poster-01.jpg',
+        url: 'https://github.com/beecart/api/raw/main/images/offer.jpg',
         link: 'https://beecart.online/beecart/categories/oral-care'
       },
       {
-        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer2.jpg',
+        url: 'https://github.com/beecart/api/raw/main/images/offer2.jpg',
         link: 'https://mydukaan.io/beecart/products/parle-real-elaichi-premium-rusk-300g'
       },
       {
-        url: 'https://github.com/sidhupk/api/raw/main/beecart/assets/offer3.jpg',
+        url: 'https://github.com/beecart/api/raw/main/images/offer3.jpg',
         link: 'https://mydukaan.io/beecart/products/santoor-soap-300g-4nx75g'
       },
     
