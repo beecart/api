60965de649bae83d6c267f8fcdbed0c57de5eb01
beecart <85115966+beecart@users.noreply.github.com>
2021-05-31 21:38:50 +0530

    Update imageList.js

--- imageList.js
+++ imageList.js
@@ -5,7 +5,7 @@ function  getImageList() {
         link: 'https://beecart.online/beecart/categories/oral-care'
       },
       {
-        url: 'https://github.com/beecart/api/raw/main/images/IMG_20200518_144904.jpg',
+        url: 'https://github.com/beecart/api/raw/main/images/IMG_20200518_144904.jpg' ,
         link: 'https://mydukaan.io/beecart/products/parle-real-elaichi-premium-rusk-300g'
       },
       {
