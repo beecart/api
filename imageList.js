31c6224e67dba0d5cdad72bd4c37dbdf48362b8b
beecart <85115966+beecart@users.noreply.github.com>
2021-06-01 14:34:22 +0530

    Update imageList.js

--- imageList.js
+++ imageList.js
@@ -1,8 +1,8 @@
 function  getImageList() {
     return [
       {
-        url: 'https://raw.githubusercontent.com/beecart/api/main/images/zoom-banner-new-393x200.jpg',
-        link: 'https://beecart.online/beecart/categories/oral-care'
+        url: 'https://raw.githubusercontent.com/beecart/api/main/images/Frame-871-393x200.jpg',
+        link: 'https://beecart.online/beecart/products/sambar-kit-2kg-sambar-powder-100gm'
       },
       {
         url: 'https://github.com/beecart/api/raw/main/images/IMG_20200518_144904.jpg' ,
